efa83df7f035d69747d95042c8baa47619d37462
da-head0 <andyourturntome@gmail.com>
2022-07-15 22:56:37 +0900

    Add test case

---
+++ test.spec.js
@@ -0,0 +1,9 @@
+describe('Default Test Set', () => {
+    it('test1 shoud be passed', () => {
+        console.log('test1 passed')
+    });
+
+    it('test2 shoud be passed', () => {
+        console.log('test2 passed')
+    });    
+});
